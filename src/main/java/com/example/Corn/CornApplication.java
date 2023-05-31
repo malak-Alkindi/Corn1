7bef3289841f80b336596510ca0d14879f4c93dd
malak-Alkindi <malakalkindi1995@gmail.com>
2023-05-31 14:18:34 +0400

    get the time

--- src/main/java/com/example/Corn/CornApplication.java
+++ src/main/java/com/example/Corn/CornApplication.java
@@ -2,7 +2,11 @@ package com.example.Corn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.TriggerContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.support.CronTrigger;
+
+import java.util.Date;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,6 +14,25 @@ public class CornApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CornApplication.class, args);
+
+		String cronn = "0 5 14 * * *";
+		CronTrigger trigger = new CronTrigger(cronn);
+		TriggerContext context = new TriggerContext() {
+
+			public Date lastScheduledExecutionTime() {
+				return null;
+			}
+
+			public Date lastActualExecutionTime() {
+				return null;
+			}
+
+			public Date lastCompletionTime() {
+				return null;
+			}
+		};
+		System.out.println("am here "+trigger.nextExecutionTime(context));
 	}
+
 
 }
--- src/main/java/com/example/Corn/shcaduling.java
+++ src/main/java/com/example/Corn/shcaduling.java
@@ -1,25 +1,47 @@
 package com.example.Corn;
 
 
+
+import it.sauronsoftware.cron4j.CronParser;
+import org.joda.time.DateTime;
+import org.springframework.scheduling.TriggerContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.support.CronExpression;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
+
+
 
 
 @Component
 public class shcaduling {
+    String cronn = "0 * * * * *";
     @Scheduled(cron = "0 * * * * *")
-    public void schedule1(){
-        String exp = "0 * * * * *";
-        DateTime now = DateTime.now();
-        CronDefinition cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(QUARTZ);
-        CronParser parser = new CronParser(cronDefinition);
-        ExecutionTime executionTime = ExecutionTime.forCron(parser.parse("* * * * * * *"));
-        DateTime nextExecution = executionTime.nextExecution(now));
-        System.out.println(CronExpression.parse(exp));
-        System.out.println("job   run");
+    public void schedule1() throws IOException {
+
+        CronTrigger trigger = new CronTrigger(cronn);
+        TriggerContext context = new TriggerContext() {
+
+            public Date lastScheduledExecutionTime() {
+                return null;
+            }
+
+            public Date lastActualExecutionTime() {
+                return null;
+            }
+
+            public Date lastCompletionTime() {
+                return null;
+            }
+        };
+      //  System.out.println("am here "+trigger.nextExecutionTime(context));
+
+
+
     }
 }
