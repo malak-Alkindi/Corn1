33e25b66407ab77f55bc2a8b997da74cd64a1a3f
malak-Alkindi <malakalkindi1995@gmail.com>
2023-05-31 16:24:07 +0400

    5

--- src/main/java/com/example/Corn/shcaduling.java
+++ src/main/java/com/example/Corn/shcaduling.java
@@ -21,11 +21,11 @@ import java.util.Date;
 @Component
 public class shcaduling {
 
-    @Scheduled(cron = "0 0 8 * * MON")
+    @Scheduled(cron = "0 0 3 1 * *")
     public void schedule1() throws IOException {
 
 
-       System.out.println("Problem 4: CRON expression for running a job every Monday at 8:00 AM:");
+       System.out.println("Problem 5: CRON expression for running a job every month on the first day at 3:00 AM:");
 
 
 
