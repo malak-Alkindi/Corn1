14ac08fa500b73da9de578df413106a17f2fc3b0
malak-Alkindi <malakalkindi1995@gmail.com>
2023-05-31 11:52:56 +0400

    .

--- src/main/java/com/example/Corn/shcaduling.java
+++ src/main/java/com/example/Corn/shcaduling.java
@@ -20,7 +20,6 @@ public class shcaduling {
         ExecutionTime executionTime = ExecutionTime.forCron(parser.parse("* * * * * * *"));
         DateTime nextExecution = executionTime.nextExecution(now));
         System.out.println(CronExpression.parse(exp));
-
         System.out.println("job   run");
     }
 }
